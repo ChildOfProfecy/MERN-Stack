d86e8cb4e768ab357db6490eda0c41052f926971
ChildOfProfecy <2010030388@klh.edu.in>
2023-03-01 19:18:41 +0530

    DataBase Connection Established

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,21 @@
 const app = require("./app")
 
 const dotenv = require("dotenv");
-// const { path } = require("./app");
+const { path } = require("./app");
+
+const connectDatabase = require("./config/database")
 
 
-dotenv.config({path:"backend/config/config.env"})
+//Config
+dotenv.config({path:"backend/config/config.env"});
+
+
+//DataBase
+connectDatabase();
+
+
+
+
 app.listen(process.env.PORT,()=>{
     console.log(`Server in wroking on http://localhost:${process.env.PORT}`);
 });
