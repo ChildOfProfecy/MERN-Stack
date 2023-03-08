d1da0467adadb2eb929705bd7d14dd6fa37adb1d
ChildOfProfecy <2010030388@klh.edu.in>
2023-03-09 00:02:54 +0530

    Created a Product In Postman and Saved in MongoDB

--- backend/controllers/productControllers.js
+++ backend/controllers/productControllers.js
@@ -1,3 +1,29 @@
-exports.getAllProducts = (req,res) => {
-    res.status(200).json({message:"Route is working fine"})
+const Product = require("../model/productModels")
+
+//Creating Product--Admin
+
+exports.createProduct = async (req,res,next)=>{
+
+    const product = await Product.create(req.body)
+    console.log("Inside Create")
+    res.status(201).json({
+        success:true,
+        product
+    })
+
 }
+
+
+//GET ALL Products
+exports.getAllProducts = async (req,res) => {
+    
+    const products = await Product.find();
+
+    res.status(200).json({
+        success:true,
+        products 
+    })
+}
+
+
+
